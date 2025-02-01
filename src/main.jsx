3a484390968e59100d3d2c75cbb4660ae0b49ab9
solaiman-siam <solaimansiam93@gmail.com>
2025-02-01 17:45:59 +0600

    some changes

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+// import MainProvider from './providers/MainProvider.jsx'
+import { RouterProvider } from 'react-router'
+import { router } from './routes/Routes.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    {/* <MainProvider> */}
+      <RouterProvider router={router}/>
+    {/* </MainProvider> */}
+  </StrictMode>,
+)
---
+++ src/providers/MainProvider.jsx
@@ -0,0 +1,25 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import AuthContextProvider from "../context/authContext/AuthContextProvider";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { HelmetProvider } from "react-helmet-async";
+import StateContextProvider from "../context/StateContext/StateContextProvider";
+
+
+const queryClient = new QueryClient();
+
+function MainProvider({ children }) {
+  return (
+    <div>
+      <QueryClientProvider client={queryClient}>
+      <AuthContextProvider>
+          <StateContextProvider>
+            <HelmetProvider>{children}</HelmetProvider>
+            </StateContextProvider>
+          </AuthContextProvider>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </div>
+  );
+}
+
+export default MainProvider;
