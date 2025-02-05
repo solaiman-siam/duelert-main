dad725e43af609aa65119977fa5fd29001200516
solaiman-siam <solaimansiam93@gmail.com>
2025-02-05 15:27:50 +0600

    some changes

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router"
 import Navbar from "../components/shared/Navbar"
 import Footer from "../components/shared/Footer"
+import { ScrollRestoration } from "react-router/dist/development"
 
 
 function MainLayout() {
   return (
     <div className="font-poppins ">
+      <ScrollRestoration/>
       <nav>
         <Navbar/>
       </nav>
