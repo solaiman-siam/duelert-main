1c222b4d50b2eb427aaf1b8056a56d297e194351
solaiman-siam <solaimansiam93@gmail.com>
2025-02-05 15:06:48 +0600

    some changes on hom epage

--- src/pages/public/Home.jsx
+++ src/pages/public/Home.jsx
@@ -1,8 +1,7 @@
 import Hero from "../../components/home-sections/Hero"
 import WhyChooseUs from "../../components/home-sections/WhyChooseUs"
-
 import BackgroundGradientImg from '../../assets/images/Background_Gradient.png'
-import KeyHighlights from "../../components/home-sections/keyHighlights"
+import KeyHighlights from '../../components/home-sections/KeyHighlights'
 import CalenderBanner from "../../components/home-sections/CalenderBanner"
 import Pricing from "../../components/home-sections/Pricing"
 import SubscribeBanner from "../../components/home-sections/SubscribeBanner"
@@ -15,7 +14,6 @@ function Home() {
       <KeyHighlights/>
       <CalenderBanner/>
       <Pricing/>
-
        <img className="w-full h-full absolute top-0 left-0 z-[-1]" src={BackgroundGradientImg} alt="" />
       </div>
       <SubscribeBanner/>
