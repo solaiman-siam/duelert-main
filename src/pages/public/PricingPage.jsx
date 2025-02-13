7e3f16916ed0e40e32c533a4e1bc602d78ce3fe4
solaiman-siam <solaimansiam93@gmail.com>
2025-02-13 16:33:21 +0600

    some changes

--- src/pages/public/PricingPage.jsx
+++ src/pages/public/PricingPage.jsx
@@ -6,9 +6,6 @@ import PricingSection from "../../components/home-sections/PricingSection"
 function PricingPage() {
   return (
     <div className="relative ">
-      <section className="py-24">
-        <Qna />
-      </section>
       <PricingSection isTitle={true} className={'py-24'}/>
       <Qna/>
       <img
