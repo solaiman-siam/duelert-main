74cb40d75f0e7d912b27f72b71a9759084b8ce89
solaiman-siam <solaimansiam93@gmail.com>
2025-02-16 11:30:10 +0600

    some changes

--- src/pages/client-dashboard/Calender.jsx
+++ src/pages/client-dashboard/Calender.jsx
@@ -1,11 +1,36 @@
+import  { useState } from "react";
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import moment from "moment";
 
+import "react-big-calendar/lib/css/react-big-calendar.css";
 
-function Calender() {
+const localizer = momentLocalizer(moment);
+
+const Calender = () => {
+  const [events, setEvents] = useState([
+    {
+      start: moment().toDate(),
+      end: moment().add(1, "days").toDate(),
+      title: "Some title"
+    },
+    {
+      start: moment().toDate(),
+      end: moment().add(1, "days").toDate(),
+      title: "Some Easy"
+    },
+  ]);
+
   return (
-    <div className="bg-white rounded-md">
-        Home Calender
+    <div className="App">
+      <Calendar
+        localizer={localizer}
+        defaultDate={new Date()}
+        defaultView="month"
+        events={events}
+        style={{ height: "100vh" }}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default Calender
+export default Calender;
